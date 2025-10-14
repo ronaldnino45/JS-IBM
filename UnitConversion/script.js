53493ec1b93a081f65cbbbc270947ae8150863f0
ronaldnino45 <ronaldninosuarez@gmail.com>
2025-10-14 15:16:49 -0400

    Agregando js a proyecto

--- UnitConversion/script.js
+++ UnitConversion/script.js
@@ -5,3 +5,20 @@ function temperature(){
     var f = (c * 9/5) + 32
     document.getElementById("fahrenheit").value = f
 }
+
+function weight(){
+    //To convert kilos to pounds
+    //Weight(Kgs) * 2.2
+    var k = document.getElementById("kilo").value;
+    var p = k * 2.2
+    document.getElementById("pounds").value = p
+}
+
+
+function distance(){
+        //To convert kms to mils
+    //Distance(Kms) * 0.62137
+    let km = document.getElementById("km").value
+    let m = km *  0.62137
+    document.getElementById("miles").value = m
+}
